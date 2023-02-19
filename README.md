b3a40859b15a7b2e6195626e8a67a36935b0e530
Asuki Kono <asukiaaa@gmail.com>
2023-02-19 21:38:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# check-serial-speed
+
+A project to check speed of serial on Ardiuno.
+
+## References
+
+- [ESP32 C3やS3の内蔵USBのシリアル通信は700Kbps台の処理速度](https://asukiaaa.blogspot.com/2023/02/esp32-c3-s3-builtin-usb-can-send-700kbps.html)
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+#ifndef SERIAL_TO_TEST
+#define SERIAL_TO_TEST Serial
+#endif
+
+#ifndef BAUDRATE
+#define BAUDRATE 115200
+#endif
+
+void setup() { SERIAL_TO_TEST.begin(BAUDRATE); }
+
+void printKiloBytes(uint32_t kiloBytes) {
+  auto start = millis();
+  for (int i = 0; i < kiloBytes * 1000; ++i) {
+    SERIAL_TO_TEST.print("a");
+  }
+  SERIAL_TO_TEST.println();
+  SERIAL_TO_TEST.flush();
+  auto diffMs = millis() - start;
+  SERIAL_TO_TEST.println("speed " + String(kiloBytes) + "K for " +
+                         String(diffMs) + "ms");
+  if (diffMs == 0) {
+    SERIAL_TO_TEST.println("so fast not to measure bps");
+  } else {
+    auto bps = (double)kiloBytes * 1000 * 1000 * 10 / diffMs;
+    if (bps > 1000000) {
+      SERIAL_TO_TEST.print(bps / 1000000);
+      SERIAL_TO_TEST.print("M");
+    } else if (bps > 1000) {
+      SERIAL_TO_TEST.print(bps / 1000);
+      SERIAL_TO_TEST.print("K");
+    } else {
+      SERIAL_TO_TEST.print(bps);
+    }
+    SERIAL_TO_TEST.print("bps");
+  }
+  SERIAL_TO_TEST.flush();
+}
+
+void loop() {
+  printKiloBytes(10);
+  delay(1000);
+  printKiloBytes(1);
+  delay(1000);
+}
